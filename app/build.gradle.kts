d96034d273b87978d24bb83efa769a85d9b3e8d5
mvideshin <mvideshin@gmail.com>
2025-06-08 23:08:40 +0200

    revert

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,10 +12,10 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file(System.getenv("KEYSTORE_PATH"))
-            storePassword = System.getenv("KEYSTORE_PASSWORD")
-            keyAlias = System.getenv("KEY_ALIAS")
-            keyPassword = System.getenv("KEY_PASSWORD")
+            storeFile = file(System.getenv("KEYSTORE_PATH") ?: "keystore.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD") ?: "defaultPass"
+            keyAlias = System.getenv("KEY_ALIAS") ?: "key-test"
+            keyPassword = System.getenv("KEY_PASSWORD") ?: "aliasPass"
         }
     }
 
