454b452477342f20199541d789e2e3554e4e9071
mvideshin <mvideshin@gmail.com>
2025-06-08 22:57:35 +0200

    add release settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,10 +12,10 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file(System.getenv("KEYSTORE_PATH") ?: "keystore.jks")
-            storePassword = System.getenv("KEYSTORE_PASSWORD") ?: "defaultPass"
-            keyAlias = System.getenv("KEY_ALIAS") ?: "alias"
-            keyPassword = System.getenv("KEY_PASSWORD") ?: "aliasPass"
+            storeFile = file(System.getenv("KEYSTORE_PATH"))
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
         }
     }
 
